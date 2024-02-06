45b921de5f63142adf047d1a78364750de312b36
Daniel Cabasa <cabasadaniel1@gmail.com>
2024-02-06 23:10:51 +0800

    Added Home Page

---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Connect</title>
+    <link rel="icon" href="./img/favicon.ico" type="image/x-icon">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="./styles/variables.css">
+    <link rel="stylesheet" href="./styles/includes.css">
+    <link rel="stylesheet" href="./styles/home.css">
+    <link rel="stylesheet" href="./styles/media_query.css">
+    <link rel="stylesheet" href="./styles/default.css">
+
+</head>
+<body>
+    <header>
+        <div class='content'>
+            <div class='logo-container'>
+                <a href="./index.php">
+                    <img src="./img/logo.png" id='logo'>
+                    <h4>House Connect</h4>
+                </a>
+            </div>
+            <div class='navigation-container'>
+                <nav>
+                    <a href='#'>ABOUT US</a>
+                    <a href='#'>HOW TO HIRE</a>
+                    <a href='#'>LOG IN</a>
+                </nav>
+            </div>
+        </div>
+    </header>
---
+++ php_files/home.php
@@ -0,0 +1,9 @@
+<main class='home'>
+    <div class='container'>
+        <div class='content'>
+            <h1>Find the <span class='orange'>perfect</span> domestic worker for you.</h1>
+            <p>Whether you're looking for a maid, nanny, gardener, cook, or driver... <br>we can be of assistance to you.</p>
+            <button>SIGN UP NOW</button>
+        </div>
+    </div>
+</main>
