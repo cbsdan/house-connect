0ae4aacbd847f83bc21e71b24c86332e43ef7323
Daniel Cabasa <cabasadaniel1@gmail.com>
2024-02-21 21:12:54 +0800

    add changes

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
     //Not logged in 
-    // include_once('./includes/header.php');
-    // include_once('./php_files/home.php');
-    // include_once('./includes/footer.php');
+    include_once('./includes/header.php');
+    include_once('./php_files/home.php');
+    include_once('./includes/footer.php');
 
     // //Worker Tab
     // include_once('./includes/header_loggedin.php');
